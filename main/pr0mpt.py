977effe8e3f3cebf5d1514636fa36ccaf57642c5
parad0x <56782193+parad0xxxx@users.noreply.github.com>
2019-10-28 01:46:47 +1100

    Update pr0mpt.py

--- main/pr0mpt.py
+++ main/pr0mpt.py
@@ -8,7 +8,6 @@ from datetime import date
 today = date.today()
 username = getpass.getuser()
 host = socket.gethostname()
-print("Welcome!")
 print(60 * "#")
 print("#                                                          #")
 print("#     Programmed by: Michael and Parad0x                   #")
@@ -17,12 +16,12 @@ print(60 * "#")
 time.sleep(0.1)
 # commands1
 while True:
-    command = str(input("$" + username + "> "))
+    command = str(input("$ " + username + " >> "))
     if command == "gethost":
         print(host)
     elif command == "youtube":
         webbrowser.open("www.youtube.com")
-    elif command == "datentime":
+    elif command == "date":
         d4 = today.strftime("%b-%d-%Y")
         print("d4 =", d4)
     elif command == "nstat":

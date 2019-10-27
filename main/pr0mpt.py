8e4725bd61e796c52b994c824b6fffe525c4a18c
parad0x <56782193+parad0xxxx@users.noreply.github.com>
2019-10-28 02:32:53 +1100

    Update pr0mpt.py

--- main/pr0mpt.py
+++ main/pr0mpt.py
@@ -5,14 +5,20 @@ import getpass
 import socket
 import webbrowser
 from datetime import date
+import platform
+
+# variables
 today = date.today()
 username = getpass.getuser()
 host = socket.gethostname()
 start_msg = 'Pr0mpt [Version 1.0.0]\n(c) 2019 parad0x & michael\n'
-
+OS = platform.system()
+architecture = platform.architecture()
+# welcome screen
 print(start_msg)
 time.sleep(0.1)
-# commands1
+
+# cmd list
 while True:
     command = str(input("$ " + username + " >> "))
     if command == "gethost":
@@ -21,7 +27,7 @@ while True:
         webbrowser.open('www.youtube.com')
     elif command == "date":
         d4 = today.strftime("%b-%d-%Y")
-        print("Date:", d4)
+        print("d4 =", d4)
     elif command == "nstat":
         os.system('netstat')
     elif command == "ipconfig":
@@ -34,6 +40,10 @@ while True:
     	quit()
     elif command == "dir":
     	os.system('dir')
+    elif command == "os":
+    	print('OS platform: ' + OS)
+    elif command == "arch":
+    	print(architecture)
     else:
     	print('')
     	print('Given command invalid\n')
